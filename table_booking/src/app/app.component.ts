524287b1a85e563cd9030c57d006ef3230c129c8
jeevan999 <jeevangorripati19@gmail.com>
2025-02-05 15:26:18 +0530

    add login component with HTML, CSS, and TypeScript; include authentication module

--- table_booking/src/app/app.component.ts
+++ table_booking/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AuthenticationModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
